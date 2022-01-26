7a89450066fd8ed570974cfa8a29de1fad06d3d0
Oscar Hackman <ohackman1@gmail.com>
2022-01-26 11:33:04 +0200

    easy invoice pdf

--- handler.js
+++ handler.js
@@ -1,9 +1,66 @@
 'use strict';
 
 const axios = require('axios');
+const easyinvoice = require('easyinvoice');
 
-const createPDF = (invoiceData) => {
-  console.log(invoiceData)
+const createPDF = (orderId, customerId, address, date) => {
+    let data = {
+      
+      "sender": {
+          "company": "Sample Corp",
+          "address": "Sample Street 123",
+          "zip": "1234 AB",
+          "city": "Sampletown",
+          "country": "Samplecountry"
+      },
+      // Your recipient
+      "client": {
+          "company": "Client Corp",
+          "address": {address},
+          "zip": "4567 CD",
+          "city": "Clientcity",
+          "country": "Clientcountry"
+          
+      },
+      "information": {
+          // Invoice number
+          "number": "2021.0001",
+          // Invoice data
+          "date": Date(date),
+          // Invoice due date
+          "due-date": "31-12-2021"
+      },
+      "products": [
+          {
+              "quantity": 2,
+              "description": "Product 1",
+              "tax-rate": 6,
+              "price": 33.87
+          },
+          {
+              "quantity": 4.1,
+              "description": "Product 2",
+              "tax-rate": 6,
+              "price": 12.34
+          },
+          
+      ],
+      // The message you would like to display on the bottom of your invoice
+      "bottom-notice": "Kindly pay your invoice within 14 days.",
+      // Settings to customize your invoice
+      "settings": {
+          "currency": "EUR", // See documentation 'Locales and Currency' for more info. Leave empty for no currency.
+          
+      },
+      // Translate your invoice to your preferred language
+      
+    };
+
+    //Create your invoice! Easy!
+    easyinvoice.createInvoice(data, function (result) {
+      //The response will contain a base64 encoded PDF file
+      console.log('PDF base64 string: ', result.pdf);
+    });
 }
 
 module.exports.createInvoice = async (event) => {
@@ -19,9 +76,8 @@ module.exports.createInvoice = async (event) => {
   const date = request.date
   
   if(orderId != '' && customerId != '' && address != '' && date != ''){
+    date = new Date(date)
     createPDF(orderId, customerId, address, date)
-  
-  
   }
 
   
